9ba34fdd4c1525fe1f7d1165be49780756395d18
MarcinBeck <marcin.beck@gmail.com>
2025-09-12 11:47:55 +0200

    Create statystyki.css

---
+++ css/statystyki.css
@@ -0,0 +1,5 @@
+main.stats-container {
+    flex-grow: 1; /* Rozciąga główną sekcję, aby wypełniła przestrzeń do stopki */
+    background-color: var(--color-light); /* Ustawia białe tło dla treści */
+    padding: 4rem 0; /* Dodaje trochę wewnętrznego marginesu dla estetyki */
+}
